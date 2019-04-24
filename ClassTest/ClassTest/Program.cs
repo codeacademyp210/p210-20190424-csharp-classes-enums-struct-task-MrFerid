27fefbb77b0b29fcd3390ac6920187df76f39346
MrFerid <farid.me93@gmail.com>
2019-04-24 23:20:41 +0400

    ss

---
+++ ClassTest/ClassTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.WriteLine("Adinizi daxil edin");
+            student.Name =  Console.ReadLine();
+
+            Console.WriteLine("Soyadinizi daxil edin");
+            student.Surname = Console.ReadLine();
+
+            Console.WriteLine("Qrupunuzu daxil edin");
+            student.Group = Console.ReadLine();
+
+            Console.WriteLine("Adiniz " + student.Name + " \nSoyadiniz: " + student.Surname + " \nQrupunuz " + student.Group);
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+    class Group
+    {
+        public string Name { get; set; }
+    }
+    class Student
+    {
+        Group group = new Group();
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group
+        {
+            get
+            {
+                return group.Name;
+            }
+            set
+            {
+                group.Name = value;
+            }
+        }
+
+    }
+}
